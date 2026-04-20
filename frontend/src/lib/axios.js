a1375986b7cb71fbb77de4a20104b95c4dff57b2
Bandarlajeevan1 <jeevanbandarla1234@gmail.com>
2026-04-20 17:23:52 +0530

    commit 2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5002/api"
+    : import.meta.env.VITE_API_URL || "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, //send the cokiees with the request...
+  withCredentials: true, // send the cookies with the request
 });
